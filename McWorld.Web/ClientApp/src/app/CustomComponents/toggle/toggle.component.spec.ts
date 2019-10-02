220abf0354e61df8face685334b1a9b2e17f92d2
Mads Illemann <mads849h@edu.eal.dk>
2019-10-02 23:03:07 +0200

    Toggle button added and done with the create Route

---
+++ McWorld.Web/ClientApp/src/app/CustomComponents/toggle/toggle.component.spec.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../../../node_modules/@types/jasmine/index.d.ts" />
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { ToggleComponent } from './toggle.component';
+
+let component: ToggleComponent;
+let fixture: ComponentFixture<ToggleComponent>;
+
+describe('toggle component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ ToggleComponent ],
+            imports: [ BrowserModule ],
+            providers: [
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ]
+        });
+        fixture = TestBed.createComponent(ToggleComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should do something', async(() => {
+        expect(true).toEqual(true);
+    }));
+});
--- McWorld.Web/ClientApp/src/app/create-route/create-route.component.ts
+++ McWorld.Web/ClientApp/src/app/create-route/create-route.component.ts
@@ -1,14 +1,29 @@
-﻿import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-create-route',
-    templateUrl: './create-route.component.html',
-    styleUrls: ['./create-route.component.scss']
+  selector: 'app-create-route',
+  templateUrl: './create-route.component.html',
+  styleUrls: ['./create-route.component.scss']
 })
 /** CreateRoute component*/
 export class CreateRouteComponent {
-    /** CreateRoute ctor */
-    constructor() {
 
-    }
+  private ferry: boolean = false;
+  private toll: boolean = false;
+  private motorway: boolean = false;
+
+  constructor() {
+
+  }
+
+  //Switch to select som thing for the route.
+  MotorwaySwitch(value) {
+    this.motorway = value;
+  }
+  TollSwitch(value) {
+    this.toll = value;
+  }
+  FerrySwitch(value) {
+    this.ferry = value;
+  }
 }
