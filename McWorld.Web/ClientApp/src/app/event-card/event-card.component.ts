f3680bf5a26893e473761be681ef3c4a2372121a
Mads Illemann <mads849h@edu.eal.dk>
2019-10-06 23:39:47 +0200

    Button on event card added

--- McWorld.Web/ClientApp/src/app/event-card/event-card.component.ts
+++ McWorld.Web/ClientApp/src/app/event-card/event-card.component.ts
@@ -1,14 +1,15 @@
-﻿import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-event-card',
-    templateUrl: './event-card.component.html',
-    styleUrls: ['./event-card.component.scss']
+  selector: 'app-event-card',
+  templateUrl: './event-card.component.html',
+  styleUrls: ['./event-card.component.scss']
 })
 /** EventCard component*/
 export class EventCardComponent {
-    /** EventCard ctor */
-    constructor() {
+  btntext: string = ´"Show Event";
 
-    }
+  constructor() {
+
+  }
 }
