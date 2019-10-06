8070a7e23343b558e08de7108b30eee4371a4b2b
Mads Illemann <mads849h@edu.eal.dk>
2019-10-06 22:05:14 +0200

    made a universal button

--- McWorld.Web/ClientApp/src/app/CustomComponents/btn-mc-world/btn-mc-world.component.ts
+++ McWorld.Web/ClientApp/src/app/CustomComponents/btn-mc-world/btn-mc-world.component.ts
@@ -2,14 +2,15 @@ import { Component, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-btn-mc-world',
-  template: `<button class="CreateBtn">
-      <p>{{ btnText }}</p>
-      </button>`,
+  templateUrl: './btn-mc-world.component.html',
   styleUrls: ['./btn-mc-world.component.scss']
 })
 /** BtnMcWorld component*/
 export class BtnMcWorldComponent {
+
   @Input() btnText: string;
+  @Input() btnWidth: string;
+
   constructor() {
 
   }
