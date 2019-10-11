b48b62e8a8081a48acdb23db227335b3f505a2e3
Mads Illemann <mads849h@edu.eal.dk>
2019-10-11 19:37:07 +0200

    Eventlist added, så there kan be more event cards showed

---
+++ McWorld.Web/ClientApp/src/app/event-list/event-list.component.spec.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../../node_modules/@types/jasmine/index.d.ts" />
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { EventListComponent } from './event-list.component';
+
+let component: EventListComponent;
+let fixture: ComponentFixture<EventListComponent>;
+
+describe('Event-List component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ EventListComponent ],
+            imports: [ BrowserModule ],
+            providers: [
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ]
+        });
+        fixture = TestBed.createComponent(EventListComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should do something', async(() => {
+        expect(true).toEqual(true);
+    }));
+});
