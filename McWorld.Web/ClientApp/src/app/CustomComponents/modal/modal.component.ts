b3f82e1decdc9f3a77545af188b7a4ae7e5108c1
Mads Illemann <mads849h@edu.eal.dk>
2019-10-20 13:46:43 +0200

    Change the editmodal to a gloabl component

--- McWorld.Web/ClientApp/src/app/CustomComponents/modal/modal.component.ts
+++ McWorld.Web/ClientApp/src/app/CustomComponents/modal/modal.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-modal',
@@ -7,8 +7,16 @@
 })
 /** Modal component*/
 export class ModalComponent {
+  @Input() hidden: boolean;
+  @Output() toggled: EventEmitter<boolean> = new EventEmitter();
+  @Input() modalWidth: string;
+
     /** Modal ctor */
     constructor() {
 
-    }
+  }
+  onClick() {
+    this.hidden = !this.hidden
+    this.toggled.emit(this.hidden);
+  }
 }
