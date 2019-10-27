f26adaa061b7bda7be51bb346470d83fffe8e017
Mads Illemann <mads849h@edu.eal.dk>
2019-10-27 23:30:57 +0100

    Minor change event card

--- McWorld.Web/ClientApp/src/app/app.server.module.ts
+++ McWorld.Web/ClientApp/src/app/app.server.module.ts
@@ -6,6 +6,6 @@ import { AppModule } from './app.module';
 
 @NgModule({
   imports: [AppModule, ServerModule, ModuleMapLoaderModule],
-  bootstrap: [AppComponent, ['ui.bootstrap']]
+  bootstrap: [AppComponent]
 })
 export class AppServerModule { }
