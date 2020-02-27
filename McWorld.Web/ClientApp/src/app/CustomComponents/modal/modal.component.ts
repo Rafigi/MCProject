a8d1ec0e8a6863824a34cab5baac9fd9204d1872
Mads Illemann <mads849h@edu.eal.dk>
2020-02-27 15:16:37 +0100

    Modal design changed

--- McWorld.Web/ClientApp/src/app/CustomComponents/modal/modal.component.ts
+++ McWorld.Web/ClientApp/src/app/CustomComponents/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-modal',
@@ -6,13 +6,34 @@ import { Component, Input } from '@angular/core';
     styleUrls: ['./modal.component.scss']
 })
 /** Modal component*/
-export class ModalComponent {
-  @Input() hidden: boolean;
-  @Input() modalWidth: string;
+export class ModalComponent implements OnInit {
+ show: boolean = false;
+  private element: any;
 
-    /** Modal ctor */
-    constructor() {
+  /** Modal ctor */
+  constructor(private el: ElementRef) {
+    this.element = el.nativeElement;
+  }
 
+  ngOnInit(): void {
+
+    // move element to bottom of page (just before </body>) so it can be displayed above everything else
+    document.body.appendChild(this.element);
+
+    // close modal on background click
+    this.element.addEventListener('click', el => {
+      if (el.target.className == 'modalBox') {
+        this.CloseModal();
+      }
+    });
+  }
+
+  OpenModal() {
+    this.show = true;
+  }
+
+  CloseModal() {
+    this.show = false;
   }
 
 }
