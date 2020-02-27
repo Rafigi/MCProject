9fb4ea6899036fe217969daba059fdb1ef8b9475
Mads Illemann <mads849h@edu.eal.dk>
2020-02-27 16:36:36 +0100

    Change home, so the new modal can be used properly

--- McWorld.Web/ClientApp/src/app/home/home.component.ts
+++ McWorld.Web/ClientApp/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { ModalComponent } from '../CustomComponents/modal/modal.component';
 
 @Component({
   selector: 'app-home',
@@ -6,50 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 /** Home component*/
-export class HomeComponent {
-  _InfoModalhidden: boolean = false;
-  _changePasswordModalhidden: boolean = false;
-  _EditEmailModalhidden: boolean = false;
-  btnDisabled: boolean = false;
+export class HomeComponent implements OnInit {
+
+  ngOnInit(): void {
+
+  }
+  
   /** Home ctor */
   constructor() {
 
-  }
-
-  ChangePassword() {
-    console.log("Password is changes!");
-    this.ShowHideChangePasswordModal();
-  }
-  CloseChangePassword() {
-    this.ShowHideChangePasswordModal();
-  }
-
-  EditEmail() {
-    console.log("Email is changes!");
-    this.ShowHideEditEmailModal();
-  }
-
-  CloseEditEmail() {
-    this.ShowHideEditEmailModal();
-  }
-
-  ShowHideInfoModal() {
-    this._InfoModalhidden = !this._InfoModalhidden;
-  }
-
-  ShowHideChangePasswordModal() {
-    this._changePasswordModalhidden = !this._changePasswordModalhidden;
-    this.DisabledInformationButton();
-    this.ShowHideInfoModal();
-  }
-
-  ShowHideEditEmailModal() {
-    this._EditEmailModalhidden = !this._EditEmailModalhidden;
-    this.DisabledInformationButton();
-    this.ShowHideInfoModal();
-  }
-
-  DisabledInformationButton() {
-    this.btnDisabled = !this.btnDisabled;
   }
 }
